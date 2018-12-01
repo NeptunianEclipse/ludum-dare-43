2fb50a5875f3a484328b62ebd02048580b81b0ab
NeptunianEclipse <neptunianeclipse@gmail.com>
2018-12-01 23:58:32 +1300

    Added Jump ability

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -35,38 +35,3 @@ public abstract class AbilityBase : ScriptableObject
 
 }
 
-[CreateAssetMenu]
-public class MidAirJump : AbilityBase
-{
-	public override KeyCode ActivateKey => KeyCode.F;
-
-	public int NumJumps = 1;
-
-	private int jumpCount;
-	private PlayerMovement movement;
-
-	protected override void Initialize()
-	{
-		base.Initialize();
-
-		controller.Activate += OnActivate;
-
-		movement = controller.GameObject.GetComponent<PlayerMovement>();
-		movement.Grounded += OnGrounded;
-	}
-
-	public override void OnActivate()
-	{
-		if(movement.IsGrounded() == false && jumpCount < NumJumps)
-		{
-			movement.Jump();
-			jumpCount++;
-		}
-	}
-
-	private void OnGrounded()
-	{
-		jumpCount = 0;
-	}
-
-}
