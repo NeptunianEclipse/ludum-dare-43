e36bdfda299219dd443b8d72caa687c2210a966a
DysnomianC <Dysnomian.gamer@gmail.com>
2018-12-02 12:53:04 +1300

    Whoops I missed a file

---
+++ Assets/Damageable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+	public void InflictDamage(float damage)
+	{
+		Debug.Log($"This ({gameObject.name}) took {damage.ToString("N1")} damage.");
+	}
+}
