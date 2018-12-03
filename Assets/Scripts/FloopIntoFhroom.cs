180860c70077cbdd6aa21840fc59025e3ccd1ea5
DysnomianC <Dysnomian.gamer@gmail.com>
2018-12-03 15:48:37 +1300

    Floops now grow

--- Assets/Scripts/FloopIntoFhroom.cs
+++ Assets/Scripts/FloopIntoFhroom.cs
@@ -12,10 +12,13 @@ public class FloopIntoFhroom : MonoBehaviour
 
 	private Throwable myThrowable;
 
+	public Vector2 startScale = new Vector2(0.01f, 0.01f);
+	public Vector2 endScale = new Vector2(1f, 1f);
+
 	void Awake()
 	{
 		myThrowable = gameObject.GetComponent<Throwable>();
-
+		
 		if (FhroomPrefab == null) Debug.LogError($"A {gameObject.name} doesn't have a prefab set in the {nameof(FloopIntoFhroom)} component.");
 	}
 
@@ -29,10 +32,40 @@ public class FloopIntoFhroom : MonoBehaviour
 		myThrowable.Expired -= MyThrowable_Expired;
 	}
 
+	void Start()
+	{
+		transform.localScale = startScale;
+
+		var deltaScale = endScale - startScale;
+		var duration = myThrowable.MaxTimeAlive;
+		StartCoroutine(Grow(deltaScale, duration, endScale));
+	}
+
 	private void MyThrowable_Expired(object sender, System.EventArgs e)
 	{
 		Vector3 position = transform.position.NewWithChange(deltaY: HeightToAdd);
 		Quaternion rotation = FhroomPrefab.transform.rotation;
 		Instantiate(FhroomPrefab, position, rotation);
+	}
+
+	private IEnumerator Grow(Vector2 deltaScale, float totalDuration, Vector2 endingScale)
+	{
+		Vector2 totalGrowth = Vector2.zero;
+
+		while (totalGrowth.magnitude < endingScale.magnitude)
+		{
+			float duration = myThrowable.MaxTimeAlive;
+			float percentageOfGrowthSinceLastFrame = Time.deltaTime / duration;
+
+			Vector2 growth = deltaScale * percentageOfGrowthSinceLastFrame;
+			transform.localScale += (Vector3)growth;
+
+			totalGrowth += growth;
+
+			yield return null;
+		}
+
+		// Remove any rounding errors from frame times not summing to the duration exactly.
+		transform.localScale = endingScale;
 	}
 }
