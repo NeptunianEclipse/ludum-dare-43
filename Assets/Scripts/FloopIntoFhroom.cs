ef18ffde39d2b6175897a5f2a753bffdf62ed2c1
DysnomianC <Dysnomian.gamer@gmail.com>
2018-12-03 19:14:12 +1300

    Growable has been refactored out of FloopIntoFhroom

--- Assets/Scripts/FloopIntoFhroom.cs
+++ Assets/Scripts/FloopIntoFhroom.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Throwable))]
+[RequireComponent(typeof(Growable))]
 public class FloopIntoFhroom : MonoBehaviour
 {
 	public float HeightToAdd = 0f;
@@ -11,13 +12,12 @@ public class FloopIntoFhroom : MonoBehaviour
 	public GameObject FhroomPrefab;
 
 	private Throwable myThrowable;
-
-	public Vector2 startScale = new Vector2(0.01f, 0.01f);
-	public Vector2 endScale = new Vector2(1f, 1f);
-
+	private Growable myGrowable;
+	
 	void Awake()
 	{
 		myThrowable = gameObject.GetComponent<Throwable>();
+		myGrowable = gameObject.GetComponent<Growable>();
 		
 		if (FhroomPrefab == null) Debug.LogError($"A {gameObject.name} doesn't have a prefab set in the {nameof(FloopIntoFhroom)} component.");
 	}
@@ -34,11 +34,7 @@ public class FloopIntoFhroom : MonoBehaviour
 
 	void Start()
 	{
-		transform.localScale = startScale;
-
-		var deltaScale = endScale - startScale;
-		var duration = myThrowable.MaxTimeAlive;
-		StartCoroutine(Grow(deltaScale, duration, endScale));
+		myGrowable.StartGrow(myThrowable.MaxTimeAlive);
 	}
 
 	private void MyThrowable_Expired(object sender, System.EventArgs e)
@@ -48,24 +44,4 @@ public class FloopIntoFhroom : MonoBehaviour
 		Instantiate(FhroomPrefab, position, rotation);
 	}
 
-	private IEnumerator Grow(Vector2 deltaScale, float totalDuration, Vector2 endingScale)
-	{
-		Vector2 totalGrowth = Vector2.zero;
-
-		while (totalGrowth.magnitude < endingScale.magnitude)
-		{
-			float duration = myThrowable.MaxTimeAlive;
-			float percentageOfGrowthSinceLastFrame = Time.deltaTime / duration;
-
-			Vector2 growth = deltaScale * percentageOfGrowthSinceLastFrame;
-			transform.localScale += (Vector3)growth;
-
-			totalGrowth += growth;
-
-			yield return null;
-		}
-
-		// Remove any rounding errors from frame times not summing to the duration exactly.
-		transform.localScale = endingScale;
-	}
 }
